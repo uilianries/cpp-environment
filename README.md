a0d08ad57a03d5cede7b7a77d7ffc1a6429badd7
Uilian Ries <uilianries@gmail.com>
2017-01-12 10:26:38 -0200

    Added Docker install

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cpp-environment
 Create a minimal environment for C++ development
+
+NOTE: Only works on Ubuntu 16.04
--- bootstrap.sh
+++ bootstrap.sh
@@ -56,3 +56,14 @@ sudo tar zxf /tmp/CLion-2016.3.2.tar.gz -C /opt/jetbrains
 sudo mv /opt/jetbrains/clion-2016.3.2/ /opt/jetbrains/clion
 sudo ln -s /opt/jetbrains/clion/bin/clion.sh /usr/local/bin/clion
 
+echo Install Docker
+sudo apt-get install -y apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo deb https://apt.dockerproject.org/repo ubuntu-xenial main | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt-get install -y docker-engine
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+echo Done! Please, reboot your system.
